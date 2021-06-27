033ac92b44504062e837c3dbfe899f9e20988cc5
Sharath70251 <84636542+Sharath70251@users.noreply.github.com>
2021-06-27 19:07:43 +0400

    Solution\

--- main.c
+++ main.c
@@ -50,11 +50,10 @@ void swap_max(int s[], int l, int n ){
     int large=s[n],nos,flag=0;
   for(int i=n;i<l;i++){
     if(s[i]>large){
-    flag=1;
-    large=s[i];
-     nos=i;
-      }
-      }
+     flag=1;
+     large=s[i];
+     nos=i;      }
+                      }
       if(n!=l-1 && (flag==1)){
       s[nos]=s[n];
       s[n]=large;
@@ -62,10 +61,11 @@ void swap_max(int s[], int l, int n ){
       for(int i=0;i<l;i++){
     printf("%i ",s[i]);
     }
+  
     printf("\n");
          }
          
-  void ssort(int s[], int l)  {
+  void ssort(int s[], int l) {
   for(int i=0;i<l;i++)
     swap_max(s,l,i);
     }
